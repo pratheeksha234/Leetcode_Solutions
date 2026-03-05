708599afa52e79af83c07bf6181100d46f19d67f
pratheeksha234 <pratheeksham23@gmail.com>
2026-03-05 16:13:33 +0530

    Add solution for minimum operations on binary string

---
+++ Strings/Easy/1758_Alternate_bin_string.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def minOperations(self, s):
+        count1 = 0
+        count2 = 0
+        
+        for i in range(len(s)):
+            if s[i] != str(i % 2):
+                count1 += 1
+            if s[i] != str((i + 1) % 2):
+                count2 += 1
+        
+        return min(count1, count2)
