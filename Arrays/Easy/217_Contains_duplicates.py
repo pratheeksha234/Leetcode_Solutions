8860d69fb2724a4078940c28c737d9d700b7947f
pratheeksha234 <pratheeksham23@gmail.com>
2026-03-08 23:23:21 +0530

    Add containsDuplicate method to check for duplicates

---
+++ Arrays/Easy/217_Contains_duplicates.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        seen = set()
+        for i in nums:
+            if i in seen:
+                return True
+            else:
+                seen.add(i)
+        return False
