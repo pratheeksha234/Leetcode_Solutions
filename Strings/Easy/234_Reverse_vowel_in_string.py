cbc75fa0ff3a7db816d8a09efb9a35cef0755f7f
Pratheeksha M <pratheeksham23@gmail.com>
2026-03-13 16:26:53 +0530

    Implement reverse vowels function in Python

---
+++ Strings/Easy/234_Reverse_vowel_in_string.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def reverseVowels(self, s):
+        vowels = "AEIOUaeiou"
+        s = list(s)
+
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            if s[left] not in vowels:
+                left += 1
+                continue
+
+            if s[right] not in vowels:
+                right -= 1
+                continue
+
+            s[left], s[right] = s[right], s[left]
+
+            left += 1
+            right -= 1
+
+        return "".join(s)
